97f665cec90a1b30f47826cee328f4fe1c8234cf
maximus <max.wasylow@trinitymirror.com>
2019-01-02 11:44:54 +0000

    ion.functions.describe_funcction is now both a decorator and normal function

--- ion/functions.py
+++ ion/functions.py
@@ -1,14 +1,19 @@
 '''Helper functions and classes for dealing with python functions'''
 from typing import Callable, Optional
+from functools import partial
+
 
 def describe_function(
-        lambda_func: Callable,
+        lambda_func: Optional[Callable] = None,
+        *,
         name: Optional[str] = None,
         qualname: Optional[str] = None,
         doc: Optional[str] = None,
         force: bool = False
 ) -> Callable:
     '''Take a anonymous function and give it a name and a doc'''
+    if lambda_func is None:
+        return partial(describe_function, name=name, qualname=qualname, doc=doc, force=force)
     if name is not None:
         if not lambda_func.__name__ or force:
             lambda_func.__name__ = name
