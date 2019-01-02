c85ba6091a5757c3faafc6aab93fef6607404c44
maximus <max.wasylow@trinitymirror.com>
2019-01-02 13:01:17 +0000

    ion.aws s3 instead of S3

--- ion/aws.py
+++ ion/aws.py
@@ -55,9 +55,9 @@ class AWSManager:
             args = {'NextToken': next_token}
         raise ValueError(f'Could not find export value for {export_name}')
 
-    def get_file(self, *, bucket: str, key: str) -> str:
+    def get_file(self, bucket: str, key: str) -> str:
         '''Get file from S3 bucket'''
-        return self.resource('S3').Object(bucket, key).get()['Body'].read().decode('utf-8')
+        return self.resource('s3').Object(bucket, key).get()['Body'].read().decode('utf-8')
 
     def stream_logs(
             self,
