e731927dc59c443bbfbaa136e68084895ad861db
maximus <max.wasylow@trinitymirror.com>
2019-01-15 15:36:18 +0000

    filter_re now accepts keys

--- ion/stream.py
+++ ion/stream.py
@@ -1,5 +1,5 @@
 '''Helper functions, generators and classes for dealing with streams/generators'''
-from typing import Generator, TypeVar, Callable
+from typing import Generator, TypeVar, Callable, Optional, Union
 import re
 
 from .bash import colors
@@ -24,11 +24,17 @@ def re_filter(
         stream: Generator[str, None, None],
         regex: str,
         *,
-        highlight: bool = False
+        highlight: bool = False,
+        key: Optional[str] = None
 ) -> Generator[str, None, None]:
     '''Filter out events in a stream if they don't match specified regex expression'''
-    def filter_func(event: str) -> bool:
-        return re.search(regex, event)
+    if key:
+        def filter_func(event: dict) -> bool:
+            event = event[key]
+            return re.search(regex, event)
+    else:
+        def filter_func(event: str) -> bool:
+            return re.search(regex, event)
     for event in filter(stream, filter_func):
         if highlight:
             yield re.sub(f'({regex})', fr'{colors.yellow}\1{colors.reset}', event)
